83ad096309ebbce1d6f53e8e09806db6380b21c4
Askar Zinurov <askar.zinurov@flant.com>
2025-11-19 17:11:52 +0200

    Add lefthook specs

--- spec/artery/browser_spec.rb
+++ spec/artery/browser_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Artery::Browser do
   it 'has a version number' do
     expect(Artery::Browser::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
